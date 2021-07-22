acbb4d25527d05706f5da14a223ba0492677bb31
Jacob Goldverg <jgoldverg@gmail.com>
2021-07-22 16:17:06 -0400

    got ride of useless printing of stack trace and a useless logger i introduced before

--- src/main/java/org/onedatashare/transferservice/odstransferservice/service/step/sftp/SFTPWriter.java
+++ src/main/java/org/onedatashare/transferservice/odstransferservice/service/step/sftp/SFTPWriter.java
@@ -67,13 +67,11 @@ public class SFTPWriter implements ItemWriter<DataChunk> {
     }
 
     public boolean cdIntoDir(ChannelSftp channelSftp, String directory){
-        logger.info(directory);
         try {
             channelSftp.cd(directory);
             return true;
         } catch (SftpException sftpException) {
             logger.warn("Could not cd into the directory we might have made moohoo");
-            sftpException.printStackTrace();
             return false;
         }
     }
@@ -84,7 +82,6 @@ public class SFTPWriter implements ItemWriter<DataChunk> {
             return true;
         } catch (SftpException sftpException) {
             logger.warn("Could not make the directory you gave us boohoo");
-            sftpException.printStackTrace();
             return false;
         }
     }

e3f1a8f4146077c0c8f41f8b005e3fd40dcaff3f
mengyuan1112 <yuanmeng@buffalo.edu>
2021-09-29 13:00:56 -0400

    Temp complete send Get Request to in specific url

--- src/main/java/org/onedatashare/transferservice/odstransferservice/service/step/http/HttpReader.java
+++ src/main/java/org/onedatashare/transferservice/odstransferservice/service/step/http/HttpReader.java
@@ -1,4 +1,60 @@
 package org.onedatashare.transferservice.odstransferservice.service.step.http;
 
-public class HttpReader {
+import com.fasterxml.jackson.databind.util.ClassUtil;
+import org.onedatashare.transferservice.odstransferservice.model.DataChunk;
+import org.onedatashare.transferservice.odstransferservice.model.EntityInfo;
+import org.onedatashare.transferservice.odstransferservice.pools.HttpConnectionPool;
+import org.onedatashare.transferservice.odstransferservice.service.FilePartitioner;
+import org.springframework.batch.item.support.AbstractItemCountingItemStreamItemReader;
+import org.springframework.util.ClassUtils;
+
+import javax.swing.text.html.parser.Entity;
+import java.net.URI;
+import java.net.URL;
+import java.net.http.HttpClient;
+import java.net.http.HttpHeaders;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.ByteBuffer;
+
+public class HttpReader<T> extends AbstractItemCountingItemStreamItemReader<DataChunk> {
+
+    HttpClient client;
+    HttpConnectionPool httpConnectionPool;
+    EntityInfo fileInfo;
+    FilePartitioner filePartitioner;
+    int chunkSize;
+    ByteBuffer buffer;
+
+
+    public HttpReader(EntityInfo fileInfo, int chunkSize) {
+        this.setExecutionContextName(ClassUtils.getShortName(HttpReader.class));
+        this.fileInfo = fileInfo;
+        this.filePartitioner = new FilePartitioner(chunkSize);
+        this.chunkSize = chunkSize;
+        buffer = ByteBuffer.allocate(this.chunkSize);
+    }
+
+
+    @Override
+    protected DataChunk doRead() throws Exception {
+        return null;
+    }
+
+    @Override
+    protected void doOpen() throws Exception {
+        this.client = this.httpConnectionPool.borrowObject();
+        HttpRequest request = HttpRequest.newBuilder()
+                                .GET()
+                                .uri(URI.create("https://" + fileInfo.getPath() + fileInfo.getId()))
+                                .setHeader("User-Agent", "Java11 Http")
+                                .build();
+        HttpResponse<String> response = this.client.send(request, HttpResponse.BodyHandlers.ofString());
+        String header = response.body();
+    }
+
+    @Override
+    protected void doClose() throws Exception {
+
+    }
 }

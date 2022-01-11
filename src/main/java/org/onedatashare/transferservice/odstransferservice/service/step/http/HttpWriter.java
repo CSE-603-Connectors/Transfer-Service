b23a4bb1cb5343fb38635bff730c22e36b7ca3fe
mengyuan1112 <yuanmeng@buffalo.edu>
2022-01-10 20:56:23 -0500

    temp complete the Write with Put+Range header

--- src/main/java/org/onedatashare/transferservice/odstransferservice/service/step/http/HttpWriter.java
+++ src/main/java/org/onedatashare/transferservice/odstransferservice/service/step/http/HttpWriter.java
@@ -2,7 +2,14 @@ package org.onedatashare.transferservice.odstransferservice.service.step.http;
 
 import org.apache.commons.httpclient.*;
 import org.apache.commons.httpclient.auth.AuthScope;
+import org.apache.commons.httpclient.methods.InputStreamRequestEntity;
+import org.apache.commons.httpclient.methods.PutMethod;
+import org.apache.commons.httpclient.methods.RequestEntity;
 import org.apache.commons.httpclient.params.HttpConnectionManagerParams;
+import org.apache.jackrabbit.webdav.DavMethods;
+import org.apache.jackrabbit.webdav.DavMethods.*;
+import org.apache.jackrabbit.webdav.property.*;
+import org.onedatashare.transferservice.odstransferservice.constant.ODSConstants;
 import org.onedatashare.transferservice.odstransferservice.model.DataChunk;
 import org.onedatashare.transferservice.odstransferservice.model.credential.AccountEndpointCredential;
 import org.slf4j.LoggerFactory;
@@ -10,11 +17,15 @@ import org.springframework.batch.core.StepExecution;
 import org.springframework.batch.core.annotation.AfterStep;
 import org.springframework.batch.core.annotation.BeforeStep;
 import org.springframework.batch.item.ItemWriter;
+import org.apache.jackrabbit.webdav.client.methods.HttpProppatch;
 
+import java.io.FileInputStream;
 import java.nio.file.*;
 
 import java.util.*;
 import org.slf4j.Logger;
+
+import javax.xml.crypto.Data;
 
 import static org.onedatashare.transferservice.odstransferservice.constant.ODSConstants.*;
 
@@ -24,6 +35,8 @@ public class HttpWriter implements ItemWriter<DataChunk> {
     String fileName;
     String destPath;
     Path filePath;
+    HttpClient client;
+
 
 
     public HttpWriter(AccountEndpointCredential credential) {
@@ -37,43 +50,29 @@ public class HttpWriter implements ItemWriter<DataChunk> {
         this.filePath = Paths.get(this.destPath);
     }
 
-    public void initClient(URI uri, int connections) {
+    public void initClient(URI uri) {
         HostConfiguration hostConfig = new HostConfiguration();
         hostConfig.setHost(uri);
-        HttpConnectionManager connectionManager = new MultiThreadedHttpConnectionManager();
-        HttpConnectionManagerParams params = new HttpConnectionManagerParams();
-        params.setMaxConnectionsPerHost(hostConfig, connections);
-        connectionManager.setParams(params);
-        HttpClient client = new HttpClient(connectionManager);
-        Credentials creds = new UsernamePasswordCredentials("", "");
+        client = new HttpClient();
+        Credentials creds = new UsernamePasswordCredentials(destCredential.getUsername(), destCredential.getSecret());
         client.getState().setCredentials(AuthScope.ANY, creds);
-        client.setHostConfiguration(hostConfig);
     }
-/*
-    // WebDAV URL:
-    final String baseUrl = ...;
-    // Source file to upload:
-    File f = ...;
-        try{
-        HttpClient client = new HttpClient();
-        Credentials creds = new UsernamePasswordCredentials("username", "password");
-        client.getState().setCredentials(AuthScope.ANY, creds);
 
-        PutMethod method = new PutMethod(baseUrl + "/" + f.getName());
-        RequestEntity requestEntity = new InputStreamRequestEntity(
-                new FileInputStream(f));
-        method.setRequestEntity(requestEntity);
-        client.executeMethod(method);
-        System.out.println(method.getStatusCode() + " " + method.getStatusText());
-    }
- */
     @AfterStep
     public void afterStep(){
-
     }
 
     @Override
-    public void write(List<? extends DataChunk> list) throws Exception {
-
+    public void write(List<? extends DataChunk> items) throws Exception {
+        PutMethod put = new PutMethod("webdav url");
+        for(DataChunk item: items) {
+            RequestEntity requestEntity =  new InputStreamRequestEntity(new FileInputStream(Arrays.toString(item.getData())));
+            put.setRequestHeader(ODSConstants.RANGE, String.format(ODSConstants.byteRange, item.getStartPosition(), item.getStartPosition()+item.getSize()));
+            put.setRequestEntity(requestEntity);
+            client.executeMethod(put);
+        }
+        if (this.destPath == null) {
+//            this.destPath
+        }
     }
 }

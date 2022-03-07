9f0c82a2adfc0b74c518b5d9418322fbdf2dc070
Jacob Goldverg <jgoldverg@gmail.com>
2022-03-07 12:10:11 -0500

    pathing shouldbe fixed

--- src/main/java/org/onedatashare/transferservice/odstransferservice/controller/MetricsCollector.java
+++ src/main/java/org/onedatashare/transferservice/odstransferservice/controller/MetricsCollector.java
@@ -2,32 +2,26 @@ package org.onedatashare.transferservice.odstransferservice.controller;
 
 import com.google.gson.*;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.apache.commons.exec.*;
 import org.onedatashare.transferservice.odstransferservice.cron.metric.NetworkMetric;
-import org.onedatashare.transferservice.odstransferservice.model.MetaDataDTO;
-import org.onedatashare.transferservice.odstransferservice.service.DatabaseService.CrudService;
 import org.onedatashare.transferservice.odstransferservice.service.DatabaseService.metric.NetworkMetricServiceImpl;
 import org.onedatashare.transferservice.odstransferservice.utility.DataUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.io.*;
-import java.text.DateFormat;
+import java.nio.file.Paths;
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.*;
 
 /**
  * @author deepika
  */
 @Service
-@NoArgsConstructor
 @Getter
 @Setter
 public class MetricsCollector {
@@ -35,9 +29,13 @@ public class MetricsCollector {
     private static final Logger log = LoggerFactory.getLogger(MetricsCollector.class);
 
     //todo - env variable
-    private static final String SCRIPT_PATH = "/Users/DG/Documents/Courses/PDP/pmeter/src/pmeter/pmeter_cli.py";
-    private static final String REPORT_PATH = "pmeter_measure.txt";
-    private static final String TEMP = "pmeter_measure_temp.txt";
+    private String scriptPath = System.getenv("PMETER_EXEC_PATH");
+//    private static final String SCRIPT_PATH = "/Users/DG/Documents/Courses/PDP/pmeter/src/pmeter/pmeter_cli.py";
+    private String reportPath = System.getProperty("user.home");
+    private String interfaceToExecuteFrom = System.getenv("PMETER_NIC_INTERFACE");
+    private String pmeterFolderPath = Paths.get(reportPath, "/.pmeter").toString();
+//    private static final String REPORT_PATH = "pmeter_measure.txt";
+    private static final String TEMP = "pmeter_measure.txt";
 
     @Autowired
     NetworkMetricServiceImpl networkMetricService;
@@ -65,24 +63,19 @@ public class MetricsCollector {
 
     }
 
-    public static void main(String[] args) throws Exception {
-        MetricsCollector metricsCollector = new MetricsCollector();
-        metricsCollector.executeScript();
-        metricsCollector.readFile();
-    }
-
     private void saveData(NetworkMetric networkMetric){
         networkMetricService.saveOrUpdate(networkMetric);
     }
 
     //python3 src/pmeter/pmeter_cli.py measure eth0 -K
     private void executeScript() throws Exception {
-        String line = "python3 " + SCRIPT_PATH;
+        String line = "python3 " + scriptPath;
         CommandLine cmdLine = CommandLine.parse(line);
         cmdLine.addArgument("measure");
-        cmdLine.addArgument("eth0");
+        cmdLine.addArgument(this.interfaceToExecuteFrom);
         cmdLine.addArgument("-K");
-
+        cmdLine.addArgument("-N");
+        cmdLine.addArgument("-T");
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         PumpStreamHandler streamHandler = new PumpStreamHandler(outputStream);
 
@@ -101,12 +94,6 @@ public class MetricsCollector {
         }
     }
 
-    private String resolvePythonScriptPath(String filename) {
-        File file = new File(filename);
-        return file.getAbsolutePath();
-    }
-
-
     /**
      * todo - parameterize
      * @return
@@ -117,11 +104,10 @@ public class MetricsCollector {
         Date startTime = null;
         Date endTime = null;
 
-        File inputFile = new File(REPORT_PATH);
+        File inputFile = new File(reportPath);
         File tempFile = new File(TEMP);
 
-        try(Reader r = new InputStreamReader(new FileInputStream(inputFile))
-        ) {
+        try(Reader r = new InputStreamReader(new FileInputStream(inputFile))) {
             tempFile.createNewFile();
             JsonStreamParser p = new JsonStreamParser(r);
             List<Map<?, ?>> metricList = new ArrayList<>();
